6f093091192bd8a176cf3a60889b11acdb259ffe
ubuntuuser <asdf>
2022-04-08 01:05:55 +0200

    started using c as prgramming language

---
+++ Jonas/c/fitsio_test/fitsio_test.c
@@ -0,0 +1,42 @@
+
+// c file 
+#include <stdio.h>
+
+// how to build fitsio.h 
+// download http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio_latest.tar.gz from https://heasarc.gsfc.nasa.gov/docs/software/fitsio/fitsio.html
+//  >  ./configure [--prefix=/target/installation/path]
+//  >  make          (or  'make shared')
+//  >  make install  (this step is optional)
+
+// important !!! link library in makefile
+// gcc $(CFLAGS) fitsio_test.c -o fitsio_test -L./../cfitsio_latest/cfitsio-4.1.0  -lcfitsio -lm -lnsl -lsocket
+
+//                                             ^^ setting the path for all the .so files 
+#include <string.h>
+#include <stdio.h>
+// #include "fitsio.h"
+#include "./../cfitsio_latest/cfitsio-4.1.0/fitsio.h"
+
+
+int main(int argc, char *argv[])
+{
+const char * s_path_file_name_input =  "./../../../2022-03-07T21-09-10_Coordinates_FILTER_30s_Severin-W.fts";
+
+fitsfile *fptr;         
+   char card[FLEN_CARD]; 
+int status = 0,  nkeys, ii;  /* MUST initialize status */
+
+fits_open_file(&fptr, s_path_file_name_input, READONLY, &status);
+   fits_get_hdrspace(fptr, &nkeys, NULL, &status);
+
+   for (ii = 1; ii <= nkeys; ii++)  { 
+      fits_read_record(fptr, ii, card, &status); /* read keyword */
+      printf("%s\n", card);
+   }
+   printf("END\n\n");  /* terminate listing with END */
+   fits_close_file(fptr, &status);
+
+   if (status)          /* print any error messages */
+      fits_report_error(stderr, status);
+   return(status);
+}
--- Jonas/de_bayering_simple_fits.py
+++ Jonas/de_bayering_simple_fits.py
@@ -3,12 +3,17 @@ import os
 import random 
 import numpy
 from astropy.io import fits as fritz
-
+import rawpy
 
 # sometimes 
 b_os_is_windows = os.name == 'nt'
 
 s_path_file_name = './../2022-03-07T21-09-10_Coordinates_FILTER_30s_Severin-W.fts'
+s_path_file_name = './../canon_mars_green_red.CR2'
+o_img_raw = rawpy.imread(s_path_file_name).raw_image
+# print(o_img_raw.shape)
+# exit()
+
 #  s_path_output_file = __file__.split(".")[0]
 s_path_output_file = "2022-03-07T21-09-10_Coordinates_FILTER_30s_Severin-W_debayered.jpg" # static filename
 s_path_output_file_suffix = "_default"
@@ -19,147 +24,127 @@ if(b_os_is_windows):
 # o_img = cv2.imread(s_path_file_name)
 o_hdulist = fritz.open(s_path_file_name)
 
+o_header = o_hdulist[0].header
+
+# print(o_header.keys)
+# exit()
+
 o_img = o_hdulist[0].data
 
-print(o_img)
-exit()
+n_height = o_img.shape[0]
+n_width = o_img.shape[1]
+print("n_height")
+print(n_height)
+print("n_width")
+print(n_width)
 
-a_img = o_img
+# n_height_array = len(o_img)
+# n_width_array = len(o_img[0])
+# print("n_height_array")
+# print(n_height_array)
+# print("n_width_array")
+# print(n_width_array)
+
+N_ASCII_R = numpy.uint8(ord("r")) #ord gets the ascii number for char 'r'
+N_ASCII_G = numpy.uint8(ord("g"))
+N_ASCII_B = numpy.uint8(ord("b"))
+# [
+#     ["r", "g"],
+#     ["g", "b"],
+# ]
+a_color_map = numpy.array(
+        [
+            numpy.array(
+                [N_ASCII_R, N_ASCII_G]
+            ),
+            numpy.array(
+                [N_ASCII_G, N_ASCII_B]
+            ),
+        ],
+        dtype=numpy.uint8 # ascii chars R G B or r g b
+    )
+n_color_map_height = len(a_color_map)
+n_color_map_width = len(a_color_map[0])
+
+def f_s_color_by_xy(n_x, n_y):
+
+    s_color = a_color_map[n_y%n_color_map_height][n_x%n_color_map_width]
+    
+    return s_color
+
+
+a_img8 = (o_img / pow(2,8)).astype('uint8')
 n_img_height = o_img.shape[0] ## important index 0 [0] is height
 n_img_width = o_img.shape[1] ## importatnt index 1 [1] is width
-#optional resize 
-n_resize_factor = 1.0 # no resizing
-# n_resize_factor = 0.25 # 10 times smallerj
-n_img_width = int(n_img_width*n_resize_factor)
-n_img_height = int(n_img_height*n_resize_factor)
-
-o_img = cv2.resize(
-    o_img,
-    (
-        n_img_width,
-        n_img_height
-    ),
-    n_resize_factor,
-    n_resize_factor
-)
-
-# cv2.imwrite("2022-03-07T21-09-10_Coordinates_FILTER_30s_Severin-W"+"_small.jpg", o_img)
-# exit()
-# pseudo crop image if width and height is not even
-n_img_width_even = int(n_img_width/2) * 2
-n_img_height_even = int(n_img_height/2) * 2
-
-n_img_debayered_width = int(n_img_width_even/2)
-n_img_debayered_height = int(n_img_height_even/2)
 
 n_color_channels = 3
+
+o_numpy_int_type = numpy.uint8
+# o_numpy_int_type = numpy.uint16
+
+
 o_debayered_img = numpy.zeros(
     [
-        n_img_debayered_height, 
-        n_img_debayered_width, 
+        n_img_height, 
+        n_img_width, 
         n_color_channels
     ],
     dtype=numpy.uint8
 )
+a_img8_normalized = (a_img8 / pow(2,8))
 
-def f_o_get_debayered_pixel(
-    n_val1,
-    n_val2,
-    n_val3,
-    n_val4,
-    s_pattern_name
-):
-    global s_path_output_file_suffix
-    s_path_output_file_suffix = s_pattern_name
+for n_y in range(0, n_img_height): 
+    for n_x in range(0, n_img_width):
+        n_val1_normalized = a_img8_normalized[n_y][n_x]
+        s_color_current = f_s_color_by_xy(n_x, n_y)
+        # o_a_o_color = {
+        #     "r": numpy.array([0], o_numpy_int_type),
+        #     "g": numpy.array([0], o_numpy_int_type),
+        #     "b": numpy.array([0], o_numpy_int_type),
+        # }
+        # numpy.append(o_a_o_color[s_color_current], n_val1)
+        # o_debayered_pixel = numpy.array(
+        #         [
+        #             o_a_o_color["b"].mean(),
+        #             o_a_o_color["g"].mean(),
+        #             o_a_o_color["r"].mean()
+        #         ],
+        # dtype=o_numpy_int_type)
 
-    if(s_pattern_name == "rggb"): 
-        o_debayered_pixel = numpy.array(
-                [
-                    (numpy.uint8(n_val4)),# b
-                    (numpy.uint8( int((n_val2+n_val3)/2))),# g
-                    (numpy.uint8(n_val1))# r
-                ],
-        dtype=numpy.uint8)
+        a_colors_r = numpy.array([0], o_numpy_int_type)
+        a_colors_g = numpy.array([0], o_numpy_int_type)
+        a_colors_b = numpy.array([0], o_numpy_int_type)
+        # # o_a_o_color[s_color_current].append(n_val1)
+        if(s_color_current == N_ASCII_R):
+            numpy.append(a_colors_r, n_val1_normalized)
+        if(s_color_current == N_ASCII_G):
+            numpy.append(a_colors_g, n_val1_normalized)
+        if(s_color_current == N_ASCII_B):
+            numpy.append(a_colors_b, n_val1_normalized)
 
-    if(s_pattern_name == "gbbr"): 
-        o_debayered_pixel = numpy.array(
-                [
-                    (numpy.uint8(n_val1)),# b
-                    (numpy.uint8( int((n_val2+n_val3)/2))),# g 
-                    (numpy.uint8(n_val4))# r
-                ],
-        dtype=numpy.uint8)
-
-    if(s_pattern_name == "gbgr"): 
-        o_debayered_pixel = numpy.array(
-                [
-                    (numpy.uint8(n_val2)),# b
-                    (numpy.uint8(int((n_val1+n_val3)/2))),# g 
-                    (numpy.uint8(n_val4))# r
-                ],
-        dtype=numpy.uint8)
-
-    if(s_pattern_name == "nur_rot"):
-    
-        o_debayered_pixel = numpy.array(
-                [
-                    0,#b
-                    0,#g
-                    255,#r
-                ],
-        dtype=numpy.uint8)
-
-    if(s_pattern_name == "rgbg"):
-        o_debayered_pixel = numpy.array(
-                [
-                   (numpy.uint8( n_val3 )), 
-                   (numpy.uint8( (n_val2 + n_val4)/2 )), 
-                   (numpy.uint8( n_val1 ))
-                ],
-        dtype=numpy.uint8)
-
-    if(s_pattern_name == "bildrauschen"):
-        o_debayered_pixel = numpy.array(
-                [   
-                    0,#numpy.uint8(int(random.random() * pow(2,8))), 
-                    numpy.uint8(int(random.random() * pow(2,8))), 
-                    0,#numpy.uint8(int(random.random() * pow(2,8))),
-                ],
-        dtype=numpy.uint8)
-
-    return o_debayered_pixel 
-
-n_y = 0
-while n_y < n_img_height_even-2:
-    n_x = 0
-    while n_x < n_img_width_even-2:
-        n_channel_index = 0 # it is rgb but all channels have the same vualue anyway, the image is black white
-        n_val1 = int(a_img[n_y][n_x][n_channel_index])
-        n_val2 = int(a_img[n_y][n_x+1][n_channel_index])
-        n_val3 = int(a_img[n_y+1][n_x][n_channel_index])
-        n_val4 = int(a_img[n_y+1][n_x+1][n_channel_index])
-
-
-        a_debayered_pixel = f_o_get_debayered_pixel(
-            n_val1, 
-            n_val2, 
-            n_val3, 
-            n_val4, 
-            "bildrauschen"
-        )
         
-        o_debayered_img[int(n_y/2)][int(n_x/2)] = a_debayered_pixel
+        o_debayered_img[n_y][n_x][0] = n_val1_normalized*pow(2,8) # b
+        o_debayered_img[n_y][n_x][1] = 0 # g
+        o_debayered_img[n_y][n_x][2] = 0 # r
         # print(a_debayered_pixel)
-        n_x = n_x + 2
+    if(n_y%100==0):
+        print("x|y: "+str(n_x)+"|"+str(n_y))
 
-    n_y = n_y + 2 
 
 
-print(s_path_output_file)
+# for n_y in range(0, n_img_height): 
+#     for n_x in range(0, n_img_width):
+#         o_debayered_img[n_y][n_x][0] = random.random()*255
+#         o_debayered_img[n_y][n_x][1] = random.random()*255
+#         o_debayered_img[n_y][n_x][2] = random.random()*255
 
-print(
-    cv2.imwrite(s_path_output_file+"_"+s_path_output_file_suffix+".jpg", o_debayered_img)
-)
+
+
+s_path_imwrite = s_path_output_file+"_"+s_path_output_file_suffix+".jpg"
+b_written = cv2.imwrite(s_path_imwrite, o_debayered_img)
+if(b_written):
+    print("image written to "+ s_path_imwrite)
+
     
 
 # print(o_img)
