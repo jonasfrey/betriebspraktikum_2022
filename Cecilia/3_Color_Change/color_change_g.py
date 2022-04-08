a149b5b1cff1da1225d99945c0b3b5e03119bdc6
unknown <cecilia.trautmann@mygymer.ch>
2022-04-08 11:22:23 +0200

    color change: r, g, b, with value who defines how much r, g, b should be changed

---
+++ Cecilia/3_Color_Change/color_change_g.py
@@ -0,0 +1,54 @@
+import cv2 
+import os
+import random 
+
+b_os_is_windows = os.name == 'nt'
+
+s_path_file_name = './../../2019-10-26T03-26-55_M51_Clear_280s_big.jpg'
+if(b_os_is_windows):
+    s_path_file_name = "\\".join(s_path_file_name.split("/"))
+
+o_img = cv2.imread(s_path_file_name)
+
+n_saturation = 0.3 #this value reduces R B so that G shines brighter
+
+def f_set_pixel_value(a_pixel, n_value):
+    a_pixel[0] = n_value * n_saturation # b
+    a_pixel[1] = n_value # g
+    a_pixel[2] = n_value * n_saturation # r
+
+a_img = o_img
+n_img_width = o_img.shape[0]
+n_img_height = o_img.shape[1]
+
+n_below_blacklevel = 255/19
+n_pixel_count_below_blacklevel = 0
+
+# loop over the image, pixel by pixel
+for n_y in range(0,n_img_height):
+    for n_x in range(0,n_img_width):
+        # threshold the pixel 
+        a_pixel = a_img[n_y][n_x]
+        n_value = a_pixel[0]
+
+        n_value = int(n_value)
+        n_value_normalized = n_value / 32
+        
+        f_set_pixel_value(
+                a_pixel,
+                min(n_value_normalized * n_value * 0.2, 255)
+            )
+       
+        if n_value < n_below_blacklevel:
+            f_set_pixel_value(a_pixel,0)
+            n_pixel_count_below_blacklevel += 1
+
+        if n_value > n_below_blacklevel:
+            f_set_pixel_value(
+                a_pixel,
+                min(n_value_normalized * 255, 255)
+            )
+
+b = cv2.imwrite(__file__.split(".")[0] +".jpg", o_img)
+b = cv2.imwrite("color_change_g.jpg", o_img)
+print(b)
